ae8075bddf3b336d66cf217b0ffae6bb2021afa4
Solano de Zuasnabar <sdezuasnabar@gmail.com>
2024-02-18 19:23:27 -0300

    modificacion en CORS

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ import path from 'path';
 
 initMongoDB()
 const app = express()
-app.use(cors({ credentials: true, origin: ['http://localhost:5173', 'https://backend-blogggers.onrender.com/'] }))
+app.use(cors({ credentials: true, origin: ['http://blogggers.onrender.com/', 'https://backend-blogggers.onrender.com/'] }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
