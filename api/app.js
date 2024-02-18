dcdb993bde730a235eb10eca1946edb99887f37a
Solano de Zuasnabar <sdezuasnabar@gmail.com>
2024-02-17 23:12:56 -0300

    intentnado deploy de nuevo

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ import path from 'path';
 
 initMongoDB()
 const app = express()
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
+app.use(cors({ credentials: true, origin: ['http://localhost:5173', 'https://backend-blogggers.onrender.com'] }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
